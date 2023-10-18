89f82bb68e400b3c8ee43afddd55fe2840e94004
Carlos Sosa <isek7or@gmail.com>
2023-10-17 20:10:32 -0700

    done

--- public/index.js
+++ public/index.js
@@ -1,10 +1,9 @@
-async function main(){
+async function main() {
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
     const response = await fetch('http://localhost:9001/counter');
-
     const result = await response.json();
     
     let countValue = result.value;
@@ -12,11 +11,29 @@ async function main(){
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+        fetch('http://localhost:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: countValue
+            })
+        })
     }
 
-    function decrement(){
+    function decrement() {
         countValue--;
         countContainer.textContent = countValue;
+        fetch('http://localhost:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: countValue
+            })
+        })
     }
 
     incrementButton.addEventListener('click', increment);
